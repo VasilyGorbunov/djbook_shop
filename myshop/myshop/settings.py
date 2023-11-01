eea8ac0e6c0dfef4bf7a38f3ddbd8943808e786f
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-01 17:29:00 +0300

    411

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
 
 
